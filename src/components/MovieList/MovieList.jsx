df0893e2cd0eb0e487edf20679f8a4884fc46f9a
Vlad Chudin <chudintttks@gmail.com>
2026-04-01 13:18:23 +0200

    fix bugs

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,9 @@
-import { MovieCard } from '../MovieCard';
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
-    {movies.map((movie) => (
+    {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
